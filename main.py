43aa223b87feb450c35eb0063e2d1420bb0bd78e
TaH4ukHero <kosov.misha2016@yandex.ru>
2023-02-18 18:27:30 +0300

    Варианты выбора

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from flask import Flask, url_for
 
 app = Flask(__name__)
-
-@app.route('/astronaut_selection', methods=['POST', 'GET'])
-def form_sample():
+@app.route('/choice/<planet_name>')
+def form_sample(planet_name):
     return f'''<!doctype html>
                     <html lang="en">
                       <head>
@@ -13,103 +12,19 @@ def form_sample():
                         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
                         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
                         crossorigin="anonymous">
-                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                        <title>Отбор астронавтов</title>
+                        <title>Варианты выбора</title>
                       </head>
                       <body>
-                        <div>
-                            <form class="login_form" method="post">
-                                <input type="surname" class="form-control" id="surname" aria-describedby="surnameHelp" placeholder="Фамилия" name="surname">
-                                <input type="name" class="form-control" id="name" placeholder="Имя" name="name">
-                                <input type="email" class="form-control" id="email" placeholder="Email" email="email">
-                                <div class="form-group">
-                                    <label for="classSelect">Образование</label>
-                                    <select class="form-control" id="classSelect" name="class">
-                                      <option>Начальное</option>
-                                      <option>Среднее</option>
-                                      <option>Высшее</option>
-                                    </select>
-                                 </div>
-                                <div class="form-group">
-                                    <label for="form-check">Выбор основной профессии</label>
-                                    <div class="form-check">
-                                      <input class="form-check-input" type="radio" name="profession" id="first" value="profession" checked>
-                                      <label class="form-check-label" for="first">
-                                        Инженер-Исследователь
-                                      </label>
-                                    </div>
-                                    <div class="form-check">
-                                      <input class="form-check-input" type="radio" name="profession" id="second" value="profession" checked>
-                                      <label class="form-check-label" for="second">
-                                        Инженер-строитель
-                                      </label>
-                                    </div>
-                                    <div class="form-check">
-                                      <input class="form-check-input" type="radio" name="profession" id="third" value="profession" checked>
-                                      <label class="form-check-label" for="third">
-                                        Пилот
-                                      </label>
-                                    </div>
-                                    <div class="form-check">
-                                      <input class="form-check-input" type="radio" name="profession" id="fourth" value="profession" checked>
-                                      <label class="form-check-label" for="fourth">
-                                        Метереолорог
-                                      </label>
-                                    </div>
-                                    <div class="form-check">
-                                      <input class="form-check-input" type="radio" name="profession" id="fivth" value="profession" checked>
-                                      <label class="form-check-label" for="fivth">
-                                        Инженер по жизниобеспечению
-                                      </label>
-                                    </div>
-                                    <div class="form-check">
-                                      <input class="form-check-input" type="radio" name="profession" id="sixth" value="profession" checked>
-                                      <label class="form-check-label" for="sixth">
-                                        Инженер по радиционной защите
-                                      </label>
-                                    </div>
-                                    <div class="form-check">
-                                      <input class="form-check-input" type="radio" name="profession" id="seventh" value="profession" checked>
-                                      <label class="form-check-label" for="seventh">
-                                        Врач
-                                      </label>
-                                    </div>
-                                    <div class="form-check">
-                                      <input class="form-check-input" type="radio" name="profession" id="eightth" value="profession" checked>
-                                      <label class="form-check-label" for="eightth">
-                                        Экзобиолог
-                                      </label>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="form-check">Пол</label>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
-                                          <label class="form-check-label" for="male">
-                                            Мужской
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
-                                          <label class="form-check-label" for="female">
-                                            Женский
-                                          </label>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="about">Мотивация</label>
-                                    <textarea class="form-control" id="about" rows="3" name="about"></textarea>
-                                </div>
-                                <div class="form-group form-check">
-                                    <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
-                                    <label class="form-check-label" for="acceptRules">Готов остаться на Марсе?</label>
-                                </div>
-                                <button type="submit" class="btn btn-primary">Отправить</button>
-                            </form>
-                        </div>
+                        <h1 class="alert alert-primary" role="alert"> Мое предложение: {planet_name.capitalize()}
+                        <h2 class="alert alert-secondary" role="alert"> Эта земля близка к Земле:; </h2>
+                        <h2 class="alert alert-success" role="alert"> На ней много необходимых ресурсов; </h2>
+                        <h2 class="alert alert-danger" role="alert"> На ней есть небольшое 
+                        магнитное поле; </h2>
+                        <h2 class="alert alert-warning" role="alert"> Наконец, она просто 
+                        красивая; </h2>
                       </body>
                     </html>'''
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8000, host='127.0.0.1')
