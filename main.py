5e27407fdd7e5334cdcd733163e3444bb58c0e31
TaH4ukHero <kosov.misha2016@yandex.ru>
2023-02-18 18:32:09 +0300

    Результат отбора

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/choice/<planet_name>')
-def form_sample(planet_name):
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def form_sample(nickname, level, rating):
     return f'''<!doctype html>
                     <html lang="en">
                       <head>
@@ -14,16 +14,18 @@ def form_sample(planet_name):
                         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
                         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
                         crossorigin="anonymous">
-                        <title>Варианты выбора</title>
+                        <title>Результаты</title>
                       </head>
                       <body>
-                        <h1 class="alert alert-primary" role="alert"> Мое предложение: {planet_name.capitalize()}
-                        <h2 class="alert alert-secondary" role="alert"> Эта земля близка к Земле:; </h2>
-                        <h2 class="alert alert-success" role="alert"> На ней много необходимых ресурсов; </h2>
-                        <h2 class="alert alert-danger" role="alert"> На ней есть небольшое 
-                        магнитное поле; </h2>
-                        <h2 class="alert alert-warning" role="alert"> Наконец, она просто 
-                        красивая; </h2>
+                        <h1 class="alert alert-primary" role="alert"> Мое предложение: Результаты 
+                        отбора </h1>
+                        <h2 class="alert alert-secondary" role="alert"> Претендента на участие в 
+                        миссии {nickname} </h2>
+                        <h2 class="alert alert-success" role="alert"> Поздравляем! Ваш рейтинг 
+                        после {level} этапа отбора 
+                        </h2>
+                        <h2 class="alert alert-danger" role="alert"> Составляет {rating}</h2>
+                        <h2 class="alert alert-warning" role="alert"> Желаем удачи </h2>
                       </body>
                     </html>'''
 
