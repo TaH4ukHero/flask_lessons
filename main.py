d0c9265497c0156090d6e54fb65421a842f09f41
TaH4ukHero <kosov.misha2016@yandex.ru>
2023-02-20 22:49:22 +0300

    Двойная Защита

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'g'
+
 
 class LoginForm(FlaskForm):
     id_astro = StringField('', validators=[DataRequired()])
@@ -13,12 +14,14 @@ class LoginForm(FlaskForm):
     password_captain = PasswordField('', validators=[DataRequired()])
     submit = SubmitField('Доступ')
 
+
 @app.route('/login')
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         return
     return render_template("double_protection.html", form=form, title='Авторизация')
+
 
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
